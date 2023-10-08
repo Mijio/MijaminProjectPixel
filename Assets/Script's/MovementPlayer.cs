3a599af308941df6de01e4e02bd377cb326ff3b0
Mijio <91673971+Mijio@users.noreply.github.com>
2023-10-08 22:55:20 +0300

    AttackIsNowWork on "E"! An you can smoothly move >:3

--- Assets/Script's/MovementPlayer.cs
+++ Assets/Script's/MovementPlayer.cs
@@ -6,19 +6,34 @@ public class MovementPlayer : MonoBehaviour
 {
     public float Speed = 6;
     private Rigidbody2D rb;
+    public Animator anim;
+    public SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
+    public Vector2 moveVector;
     void FixedUpdate()
     {
+        Flip();
+        Walk();
+    }
+    void Walk()
+    {
+        moveVector.x = Input.GetAxis("Horizontal");
+        anim.SetFloat("moveX", Mathf.Abs(moveVector.x));
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
-
         rb.velocity = new Vector2(Speed * x, Speed * y);
+    }
+    void Flip()
+    {
+        sr.flipX = moveVector.x < 0;
     }
 }
