19ccd0e42837fd3e4b3d276cf7d60897516b74b7
QuantumTrolling <st097173@student.spbu.ru>
2023-10-10 21:06:49 +0300

    Patrolling_enemy

--- Assets/Script's/EnemyManager.cs
+++ Assets/Script's/EnemyManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
+    public Transform[] Points;
+    private int randomSpot;
+    private float waitTime;
+    private float Startwaittime;
+
     private Rigidbody2D physic;
 
     public Transform player;
@@ -13,7 +18,12 @@ public class EnemyManager : MonoBehaviour
     public float speed;
     public float agroRange;
 
-    void Start() => physic = GetComponent<Rigidbody2D>();
+    void Start()
+    {
+        physic = GetComponent<Rigidbody2D>();
+        waitTime = Startwaittime;
+        randomSpot = Random.Range(0, Points.Length);
+    }
 
     void Update()
     {
@@ -23,6 +33,12 @@ public class EnemyManager : MonoBehaviour
         {
             goblin_animator.SetFloat("HorizontalMove", 1);
             StartHunting();
+        }
+        else if ((distToPlayer > agroRange))
+        {
+            goblin_animator.SetFloat("HorizontalMove", 1);
+            StopHunting();
+            Walking();
         }
         else
         {
@@ -54,10 +70,35 @@ public class EnemyManager : MonoBehaviour
             physic.velocity = new Vector2(speed, speed);
             transform.localScale = new Vector2(1, 1);
         }
+        else if((player.position.x == transform.position.x) && (player.position.y < transform.position.y))
+        {
+            physic.velocity = new Vector2(0, -speed);
+        }
+        else if((player.position.x == transform.position.x) && (player.position.y > transform.position.y))
+        {
+            physic.velocity = new Vector2(0, speed);
+        }
     }
 
     void StopHunting()
     {
         physic.velocity = new Vector2(0, 0);
+    }
+
+    void Walking()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Points[randomSpot].position, (speed/2) * Time.deltaTime);
+        if (Vector2.Distance(transform.position, Points[randomSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                waitTime = Startwaittime;
+                randomSpot = Random.Range(0, Points.Length);
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
     }
 }
